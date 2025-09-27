18cdf0e82814e62a7c2368202a763ac1f9de4251
Uday Singh <uday.singh@firsthive.com>
2025-09-27 17:27:22 +0530

    Initialized ecommerce backend

---
+++ src/main/java/com/uday/UrbanKart/UrbanKartApplication.java
@@ -0,0 +1,13 @@
+package com.uday.UrbanKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanKartApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/uday/UrbanKart/model/Category.java
@@ -0,0 +1,23 @@
+package com.uday.UrbanKart.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nama;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
